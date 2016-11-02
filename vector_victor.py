2ac06c1a1b1f5950ff004c05375c78200833e198
Cameron Moeller <cameron.moeller@gmail.com>
2016-11-02 16:46:35 -0400

    on to advanced. got shape working for matrices

---
+++ vector_victor.py
@@ -0,0 +1,57 @@
+from nose.tools import raises
+
+class ShapeError(Exception):
+    pass
+
+def shape(vector):
+    columns = len(vector)
+    try:
+        rows = len(vector[0])
+        return (columns, rows)
+    except:
+        return (columns, )
+
+def vector_add(vector1, vector2):
+    if shape(vector1) == shape(vector2):
+        new_list = [a + b for a, b in zip(vector1, vector2)]
+        return new_list
+    else:
+        raise ShapeError
+
+def vector_add_is_commutative():
+    pass
+
+def vector_sub(vector1, vector2):
+    if shape(vector1)==shape(vector2):
+        new_list = [a - b for a, b in zip(vector1, vector2)]
+        return new_list
+    else:
+        raise ShapeError
+
+def vector_sum(*args):
+    shape_list = [shape(vector) for vector in args]
+    if min(shape_list) == max(shape_list):
+        new_list = []
+        new_list = [sum(x) for x in zip(*args)]
+        return new_list
+    else:
+        raise ShapeError
+
+def dot(vector1, vector2):
+    if shape(vector1) == shape(vector2):
+        new_list = [a * b for a, b in zip(vector1, vector2)]
+        return sum(new_list)
+    else:
+        raise ShapeError
+
+def vector_multiply(vector1, scalar):
+    new_list = [scalar * x for x in vector1]
+    return new_list
+
+def vector_mean(*args):
+    mean_list =  [x *(1/len(args)) for x in vector_sum(*args)]
+    return mean_list
+
+def magnitude(vector1):
+    squares_added = [x**2 for x in vector1]
+    return (sum(squares_added))**(1/2)
